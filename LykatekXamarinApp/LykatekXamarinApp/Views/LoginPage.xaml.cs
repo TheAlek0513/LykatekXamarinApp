9562e6cc0ec91a1f7eca538d19ba1126d4741853
Aleksander Mosekjær Møller <1080633@ucn.dk>
2021-10-20 08:56:59 +0200

    added method to get all produkts

--- LykatekXamarinApp/LykatekXamarinApp/Views/LoginPage.xaml.cs
+++ LykatekXamarinApp/LykatekXamarinApp/Views/LoginPage.xaml.cs
@@ -55,7 +55,7 @@ namespace LykatekXamarinApp.Views
                     Settings.DebtorId = authenticatedContactPerson.DCAccount;
 
                     App.SettingsDatabase.SaveSettings();
-                    var syncResult = await Utillity.SyncAll();
+                   var syncResult = await Utillity.SyncAll();
 
                     if (!syncResult)
                     {
@@ -64,7 +64,7 @@ namespace LykatekXamarinApp.Views
                         LoginButton.IsEnabled = true;
                         return;
                     }
-
+                    var testItems = await Utillity.getInvItemWithPictures();
                     Application.Current.MainPage = new NavigationPage(new StartPage());
                     LoginActivityIndicator.IsRunning = false;
                 }
